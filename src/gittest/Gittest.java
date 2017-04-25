fce47d0399578989a01f2df70f57ce284475d9ba
monde <monde@monde-VirtualBox>
2017-04-25 15:31:49 +0200

    some changes.

--- src/gittest/Gittest.java
+++ src/gittest/Gittest.java
@@ -1,12 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gittest;
 
 /**
- *
+ * Adding a new comment that will appear on github
  * @author monde
  */
 public class Gittest {
